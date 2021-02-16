b17171f74392f45619588a9a0c3ac43b2afee10a
mjschuette4 <mjschuette4@gmail.com>
2021-02-16 10:00:04 -0600

    Reset all front end work for now

--- friend_finder/src/App.js
+++ friend_finder/src/App.js
@@ -1,18 +1,8 @@
 import React from 'react'; 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import home from './components/home';
-import login from './components/login';
-import register from './components/register';
 
 function App() {
   return (
-    <Router>
-      <Route path='/' exact component={home} />
-      <Route path='/Login' exact component={login} />
-      <Route path='/Register' exact component={register} />
-    </Router>
+    <h1>Hello There</h1>
   );
 }
 
